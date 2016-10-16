3bb13277c65e1730d7ac773f0e182303e039dfb3
Heiko Rothe <heikorothe@msn.com>
2016-10-16 17:18:59 +0200

    Added console publisher

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ require('scribe-js')();
 var config = require('config');
 var console = process.console;
 
-var MQTTPublisher = require('./components/mqtt');
-var BLEScanner = require('./components/ble');
-var Temper = require('./components/temper');
-var GPIO = require('./components/gpio');
+var Publisher = require('./components/publisher');
+if (config.get('ble.enabled')) {
+    var BLEScanner = require('./components/ble');
+}
+if (config.get('temper.enabled')) {
+    var Temper = require('./components/temper');
+}
+if (config.get('gpio.enabled')) {
+    var GPIO = require('./components/gpio');
+}
 
 function RoomAssistantApp() {
     console.info('Starting Room Assistant...');
@@ -15,9 +21,7 @@ function RoomAssistantApp() {
 
 RoomAssistantApp.prototype._init = function () {
     // publishers
-    if (config.get('mqtt.enabled')) {
-        this.publisher = this._setupMQTT();
-    }
+    this.publisher = new Publisher();
 
     // components
     if (config.get('ble.enabled')) {
@@ -29,10 +33,6 @@ RoomAssistantApp.prototype._init = function () {
     if (config.get('gpio.enabled')) {
         this._setupGPIO();
     }
-};
-
-RoomAssistantApp.prototype._setupMQTT = function () {
-    return new MQTTPublisher();
 };
 
 RoomAssistantApp.prototype._setupBLE = function () {
