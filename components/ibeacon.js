db20312c7918069b047b164c79034d91e8012767
kdanthony <kevin@anthonynet.org>
2016-10-21 20:41:51 +0000

    Moved away from using accuracy for distance as directly reported and to the same kalman filter used on the BLE component for consistency and noise reduction.

--- components/ibeacon.js
+++ components/ibeacon.js
@@ -2,11 +2,14 @@ var config = require('config');
 var bleacon = require('bleacon');
 var console = process.console;
 
+var KalmanFilter = require('kalmanjs').default;
+
 var channel = config.get('ibeacon.channel');
 
 function iBeaconScanner(callback) {
     // constructor
     this.callback = callback;
+    this.kalmanManager = {};
 
     this._init();
     console.info('iBeacon scanner was initialized');
@@ -24,25 +27,57 @@ iBeaconScanner.prototype._handlePacket = function (ibeacon) {
     var whitelist = config.get('ibeacon.whitelist') || [];
     if (whitelist.length == 0 || whitelist.indexOf(ibeacon.uuid) > -1) {
 
-    id = ibeacon.uuid + '-' + ibeacon.major + '-' + ibeacon.minor
+        // default hardcoded value for beacon tx power
+        var txPower = ibeacon.measuredPower || -59;
+        var distance = this._calculateDistance(ibeacon.rssi, txPower);
+
+        id = ibeacon.uuid + '-' + ibeacon.major + '-' + ibeacon.minor
         // max distance parameter checking
         var maxDistance = config.get('ibeacon.max_distance') || 0;
         if (maxDistance == 0 || ibeacon.accuracy <= maxDistance) {
+            var filteredDistance = this._filter(id, distance);
 
             var payload = {
-        id: id,
+		id: id,
                 uuid: ibeacon.uuid,
                 major: ibeacon.major,
-        minor: ibeacon.minor,
+		minor: ibeacon.minor,
                 rssi: ibeacon.rssi,
-        distance: ibeacon.accuracy,
-        measuredpower: ibeacon.measuredPower,
-        proximity: ibeacon.proximity
+		distance: filteredDistance,
+		accuracy: ibeacon.accuracy,
+		measuredpower: ibeacon.measuredPower,
+		proximity: ibeacon.proximity
             };
 
             this.callback(channel, payload);
         }
     }
 };
+
+iBeaconScanner.prototype._calculateDistance = function (rssi, txPower) {
+    if (rssi == 0) {
+        return -1.0;
+    }
+
+    var ratio = rssi * 1.0 / txPower;
+    if (ratio < 1.0) {
+        return Math.pow(ratio, 10);
+    }
+    else {
+        return (0.89976) * Math.pow(ratio, 7.7095) + 0.111;
+    }
+};
+
+iBeaconScanner.prototype._filter = function (id, distance) {
+    if (!this.kalmanManager.hasOwnProperty(id)) {
+        this.kalmanManager[id] = new KalmanFilter({
+            R: config.get('ibeacon.system_noise') || 0.01,
+            Q: config.get('ibeacon.measurement_noise') || 3
+        });
+    }
+
+    return this.kalmanManager[id].filter(distance);
+};
+
 
 module.exports = iBeaconScanner;
