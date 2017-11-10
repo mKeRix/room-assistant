0f6b8997cb74bc608e69b46b2e7b37bebb1e2930
Manfred Martin <manfred.martin@gmail.com>
2017-11-11 00:21:31 +0100

    Add Docker files & Travis configuration

--- test/ble.js
+++ test/ble.js
@@ -1,13 +1,13 @@
-var should = require('chai').should(),
-    BLEScanner = require('../components/ble');
+var expect = require('chai').expect;
+var BLEScanner = require('../components/ble');
 
 describe('BLEScanner', function () {
     describe('constructor', function () {
         var callback = function () {};
         var scanner = new BLEScanner(callback);
 
-        scanner.callback.should.be.a('function');
-        scanner.kalmanManager.should.be.an('object');
-        scanner.kalmanManager.should.be.empty();
+        expect(scanner.callback).to.be.a('function');
+        expect(scanner.kalmanManager).to.be.an('object');
+        expect(scanner.kalmanManager).to.be.empty;
     })
 });
