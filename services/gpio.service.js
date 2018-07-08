b1157c595d4a9ada41d17378c4477521b8d1c99d
Heiko Rothe <heikorothe@msn.com>
2018-07-08 21:25:55 +0200

    Added gpio service

---
+++ services/gpio.service.js
@@ -0,0 +1,51 @@
+'use strict';
+
+const config = require('config');
+const Gpio = require('onoff').Gpio;
+
+module.exports = {
+    name: 'gpio',
+
+    settings: {
+        ports: config.get('gpio.ports')
+    },
+
+    methods: {
+        handleGpioEvent(err, value, port) {
+            if (err) {
+                this.logger.error(`The gpio watcher raised an error: ${err}`);
+            }
+
+            const payload = {
+                channel: port.channel,
+                data: {
+                    value: value
+                },
+                options: {
+                    retain: port.retain
+                }
+            };
+
+            this.broker.emit('data.found', payload);
+        }
+    },
+
+    created() {
+        this.gpioMap = new Map();
+
+        this.settings.ports.forEach((port) => {
+            const gpio = new Gpio(port.pin, 'in', 'both');
+            gpio.watch((err, value) => {
+                this.handleGpioEvent(err, value, port);
+            });
+
+            this.gpioMap.set(port.pin, gpio);
+        });
+    },
+
+    stopped() {
+        this.gpioMap.forEach(function (gpio) {
+            gpio.unexport();
+        });
+    }
+};
