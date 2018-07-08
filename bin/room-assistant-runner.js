1282dd7e29e93169c213671b251f51744a3ff061
Heiko Rothe <heikorothe@msn.com>
2018-07-08 10:48:29 +0200

    Added cache of installed dependencies

--- bin/room-assistant-runner.js
+++ bin/room-assistant-runner.js
@@ -4,6 +4,7 @@ const _ = require('lodash');
 const { spawn } = require('child_process');
 const util = require('util');
 const npm = require('global-npm');
+const fs = require('fs');
 
 const dependencies = require('../dependencies');
 
@@ -18,25 +19,62 @@ function getDependencies() {
         }
         toBeInstalled = _.flatMap(toBeInstalled, (module) => module);
 
-        console.log(`Packages to be installed: ${toBeInstalled}`);
+        console.log(`Packages needed: ${toBeInstalled}`);
 
         resolve(toBeInstalled);
     });
 }
 
+function excludeCachedDependencies(toBeInstalled) {
+    return getDependencyCache()
+        .then(function (installed) {
+            console.log(`Already installed: ${installed}`);
+
+            return _.difference(toBeInstalled, installed);
+        });
+}
+
+function getDependencyCache() {
+    const readFile = util.promisify(fs.readFile);
+
+    return readFile('dependencies.cache.json')
+        .then(function (content) {
+            return JSON.parse(content);
+        })
+        .catch(function (err) {
+            Promise.resolve([]);
+        })
+}
+
+function saveDependencyCache(installed) {
+    const writeFile = util.promisify(fs.writeFile);
+
+    if (installed && installed.length > 0) {
+        return getDependencies()
+            .then(function (dependencies) {
+                return writeFile('dependencies.cache.json', JSON.stringify(dependencies));
+            })
+    }
+}
+
+function installDependencies(toBeInstalled) {
+    if (toBeInstalled.length > 0) {
+        const npmInstall = util.promisify(npm.commands.install);
+        return npmInstall(toBeInstalled);
+    }
+}
+
+function runMoleculer() {
+    spawn('moleculer-runner', process.argv.slice(2), { stdio: 'inherit'} );
+}
 
 const npmLoad = util.promisify(npm.load);
-npmLoad({loglevel: 'silent', save: false})
+npmLoad({save: false})
     .then(getDependencies)
-    .then(function (toBeInstalled) {
-        if (toBeInstalled.length > 0) {
-            const npmInstall = util.promisify(npm.commands.install);
-            return npmInstall(toBeInstalled);
-        }
-    })
-    .then(function () {
-        spawn('moleculer-runner', process.argv.slice(2), { stdio: 'inherit'} );
-    })
+    .then(excludeCachedDependencies)
+    .then(installDependencies)
+    .then(saveDependencyCache)
+    .then(runMoleculer)
     .catch(function (err) {
         console.error(err)
     });
