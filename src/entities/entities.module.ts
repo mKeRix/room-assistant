b7de024fe23612843101ff399b8e450821a3e9c3
Heiko Rothe <heikorothe@msn.com>
2020-01-24 12:41:48 +0100

    fix: Add ClusterModule to EntitiesModule dependencies

--- src/entities/entities.module.ts
+++ src/entities/entities.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { EntitiesService } from './entities.service';
+import { ClusterModule } from '../cluster/cluster.module';
 
 @Module({
+  imports: [ClusterModule],
   providers: [EntitiesService],
   exports: [EntitiesService]
 })
