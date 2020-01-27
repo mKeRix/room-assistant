09ceab370a049b51305c5803eff7d445cd88a7ef
Heiko Rothe <heikorothe@msn.com>
2020-01-27 21:06:26 +0100

    feat(cli): added verbose option
    The log level is now set to exclude debug and verbose output by default,
    but it can be turned back on by starting room-assistant with the
    --verbose option.

--- bin/room-assistant.js
+++ bin/room-assistant.js
@@ -1,2 +1,41 @@
 #!/usr/bin/env node
+const commandLineUsage = require('command-line-usage');
+const commandLineArgs = require('command-line-args');
+
+const optionDefinitions = [
+  {
+    name: 'help',
+    description: 'Display this usage guide.',
+    alias: 'h',
+    type: Boolean
+  },
+  {
+    name: 'verbose',
+    description: 'Turn on debugging output.',
+    alias: 'v',
+    type: Boolean
+  }
+];
+const usage = commandLineUsage([
+  {
+    header: 'room-assistant',
+    content:
+      'A companion client for Home Assistant to handle presence detection and sensors in multiple rooms.'
+  },
+  {
+    header: 'Options',
+    optionList: optionDefinitions
+  }
+]);
+const options = commandLineArgs(optionDefinitions);
+
+if (options.help) {
+  console.log(usage);
+  return;
+}
+
+process.env.NODE_LOG_LEVEL = options.verbose
+  ? 'verbose'
+  : process.env.NODE_LOG_LEVEL || 'production';
+
 require('../dist/main');
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_LOG_LEVEL === 'production'
+        ? ['error', 'warn', 'log']
+        : undefined
+  });
   app.enableShutdownHooks();
   await app.listenAsync(6415);
 }
