b1fd7ae863af60800ead44ecab23c968ea4c741f
Caleb B <caleb@blankemeyer.us>
2020-09-30 15:29:50 -0400

    fix(home-assistant): replace apostrophes with dashes (#282)

--- src/util/id.ts
+++ src/util/id.ts
@@ -9,5 +9,5 @@ import slugify from 'slugify';
 export function makeId(input: string): string {
   return slugify(input, {
     lower: true,
-  }).replace(/[_:]/g, '-');
+  }).replace(/[_:']/g, '-');
 }
